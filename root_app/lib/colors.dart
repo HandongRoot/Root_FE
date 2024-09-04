aa48d75171033beac7da9474ad8ed847cb885c84
naim-kim <143364203+naim-kim@users.noreply.github.com>
2024-09-04 22:56:07 +0900

    Update colors.dart

--- root_app/lib/colors.dart
+++ root_app/lib/colors.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color primaryColor = Color(0xFFebebeb); // Example primary color
   static const Color secondaryColor = Color(0xFF417FF2); // Example secondary color
-  static const Color accentColor = Color(0xFFFFD54F); // Example accent color
-  static const Color backgroundColor = Color(0xFFFFFFFF); // Example background color
+  static const Color accentColor = Color(0xFF203D77); // Example accent color
+  static const Color backgroundColor = Color(0xFFE3DEDE); // Example background color
   static const Color textColor = Color(0xFF212121); // Example text color
   static const Color iconColor = Color(0xFF417FF2); // Example icon color
   static const Color buttonColor = Color(0xFFFFFFFF); // Example button color
