a2e1f5a6420e3efd99c2f5de7d63f009fd2b6b06
naim-kim <143364203+naim-kim@users.noreply.github.com>
2024-10-22 14:00:19 +0900

    Update routes of the app

--- root_app/lib/main.dart
+++ root_app/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
-import 'components/appbar.dart'; // Import the CustomAppBar
-import 'components/navigationbar.dart'; // Import the CustomNavigationBar
-import 'search_page.dart'; // Import the search page
+import 'home_navigation.dart';
+import 'add.dart';
+import 'search_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp()); // 앱 돌릴떄 여기서 시작함
 }
 
+/*
+ * structure, theme, and routing 담당 페이지
+ */
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -15,89 +18,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(), // Set HomePage as the starting page
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(), // Use the CustomAppBar as the AppBar
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, // 2 columns in the grid
-            crossAxisSpacing: 16.0, // Space between columns
-            mainAxisSpacing: 16.0, // Space between rows
-            childAspectRatio: 3 / 2, // Aspect ratio for each grid item
-          ),
-          itemCount: 4, // Number of folder widgets
-          itemBuilder: (context, index) {
-            return FolderWidget(
-              text: 'Folder $index',
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          SearchPage()), // Navigate to search page
-                );
-              },
-            );
-          },
-        ),
-      ),
-      bottomNavigationBar:
-          CustomNavigationBar(), // Add the custom navigation bar at the bottom
-    );
-  }
-}
-
-class FolderWidget extends StatelessWidget {
-  final String text;
-  final VoidCallback onPressed;
-
-  const FolderWidget({
-    Key? key,
-    required this.text,
-    required this.onPressed,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onPressed,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1), // Light shadow
-              blurRadius: 3,
-              offset: Offset(1, 1),
-            ),
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: Container(
-            color: Colors.blue[100], // Example background color
-            child: Center(
-              child: Text(
-                text,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false, // 거슬리는 debug banner 지우는 코드
+      initialRoute: '/', // Default route
+      routes: {
+        '/': (context) =>
+            HomeNavigation(), // HomeNavigation will manage switching between HomePage and GalleryPage
+        '/add': (context) => AddPage(),
+        '/search': (context) => SearchPage(),
+      },
     );
   }
 }
