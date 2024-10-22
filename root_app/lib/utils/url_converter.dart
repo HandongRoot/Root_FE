8348deace9c4dc775ba44d8cd35494ca49af6577
naim-kim <143364203+naim-kim@users.noreply.github.com>
2024-10-22 17:04:37 +0900

    Add url converter to converto youtube shorts

--- root_app/lib/utils/url_converter.dart
+++ root_app/lib/utils/url_converter.dart
@@ -1,21 +1,24 @@
 // utils/url_converter.dart
 
-//TODO: 유투브 한정이라.. 인스타그램이랑 다른 어플들도 해봐야할듯..
+//TODO: youtube 이랑 유투브 쇼츠만 변환하도록 설정함.. 다른거 다 설정 하거나 방법 찾아봐야함.
 String getThumbnailFromUrl(String url) {
-  String? videoId = getIdFromUrl(url); // extract youtube url
+  String? videoId =
+      getIdFromUrl(url); // Extract video ID from YouTube or YouTube Shorts
 
   if (videoId != null) {
     // Return thumbnail URL
     return 'https://img.youtube.com/vi/$videoId/hqdefault.jpg';
   } else {
-    // 유툽 영상 아닌 url handling
+    // Handle non-YouTube URLs or invalid URLs
     return url;
   }
 }
 
 String? getIdFromUrl(String url) {
+  // Updated regex to handle both regular YouTube videos and YouTube Shorts
   RegExp regExp = RegExp(
-      r'(?:(?:https?:)?\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})');
+      r'(?:(?:https?:)?\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|shorts\/|embed\/|v\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})');
+
   var match = regExp.firstMatch(url);
-  return match?.group(1);
+  return match?.group(1); // Return the extracted video ID, if matched
 }
