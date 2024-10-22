2ab785979304f7da145f058437d421f20af99ca8
naim-kim <143364203+naim-kim@users.noreply.github.com>
2024-10-22 16:00:25 +0900

    Add colors.dart

--- root_app/lib/colors.dart
+++ root_app/lib/colors.dart
@@ -1,14 +1,14 @@
-// lib/colors.dart
 import 'package:flutter/material.dart';
 
+//TODO: 앱에 적용할.. 그.. color 들 모아두는 곳이랄까 그런데 일단 다 안함
+
 class AppColors {
-  static const Color primaryColor = Color(0xFFebebeb); // Example primary color
-  static const Color secondaryColor = Color(0xFF417FF2); // Example secondary color
-  static const Color accentColor = Color(0xFF203D77); // Example accent color
-  static const Color backgroundColor = Color(0xFFE3DEDE); // Example background color
-  static const Color textColor = Color(0xFF212121); // Example text color
-  static const Color iconColor = Color(0xFF417FF2); // Example icon color
-  static const Color buttonColor = Color(0xFFFFFFFF); // Example button color
-  static const Color buttonShadowColor = Color(0x80000000); // Example button shadow color
-// Add more colors as needed
+  static const Color primaryColor = Color(0xFFebebeb);
+  static const Color secondaryColor = Color(0xFF417FF2);
+  static const Color accentColor = Color(0xFF203D77);
+  static const Color backgroundColor = Color(0xFFE3DEDE);
+  static const Color textColor = Color(0xFF212121);
+  static const Color iconColor = Color(0xFF417FF2);
+  static const Color buttonColor = Color(0xFFFFFFFF);
+  static const Color buttonShadowColor = Color(0x80000000);
 }
