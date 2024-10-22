c87d26a5e44cba54f3526edfa87f0b6e811fb4f1
naim-kim <143364203+naim-kim@users.noreply.github.com>
2024-10-22 14:15:21 +0900

    Add youtube url to thumbnail url converter to a separate dart compoent

---
+++ root_app/lib/utils/url_utils.dart
@@ -0,0 +1,21 @@
+// utils/url_utils.dart
+
+//TODO: 유투브 한정이라.. 인스타그램이랑 다른 어플들도 해봐야할듯..
+String getThumbnailFromUrl(String url) {
+  String? videoId = getIdFromUrl(url); // extract youtube url
+
+  if (videoId != null) {
+    // Return thumbnail URL
+    return 'https://img.youtube.com/vi/$videoId/hqdefault.jpg';
+  } else {
+    // 유툽 영상 아닌 url handling
+    return url;
+  }
+}
+
+String? getIdFromUrl(String url) {
+  RegExp regExp = RegExp(
+      r'(?:(?:https?:)?\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})');
+  var match = regExp.firstMatch(url);
+  return match?.group(1);
+}
