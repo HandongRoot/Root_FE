30a7d190bea9a1314820a88abbe36c09949e3cd7
IMSEONGBIN506 <bins506@gmail.com>
2025-02-12 20:23:41 +0900

    .

--- root_app/lib/theme/theme.dart
+++ root_app/lib/theme/theme.dart
@@ -4,10 +4,7 @@ final ThemeData appTheme = ThemeData(
   scaffoldBackgroundColor: Colors.white,
   appBarTheme: const AppBarTheme(
     backgroundColor: Colors.white,
-    titleTextStyle: TextStyle(
-      fontWeight: FontWeight.bold,
-      fontSize: 24,
-    ),
+    
   ),
   textTheme: const TextTheme(
     bodyMedium: TextStyle(color: Colors.black),
