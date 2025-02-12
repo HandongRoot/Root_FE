c0512f442d12f8750435be0ebb3588854b057793
naimkim <naimk5451@gmail.com>
2025-02-13 00:06:20 +0900

    STYLE search 페이지 디자인 적용 완 + no search results 적용

--- root_app/lib/utils/icon_paths.dart
+++ root_app/lib/utils/icon_paths.dart
@@ -22,6 +22,7 @@ class IconPaths {
   static const String filter = '${basePath}folder_select_filter.svg';
   static const String double_arrow = '${basePath}double_arrow.svg';
   static const String search_folder = '${basePath}search_folder.svg';
+  static const String notfound_folder = '${basePath}notfound_folder.svg';
 
   static String getIcon(String iconName) {
     return '$basePath$iconName.svg';
