3ebfdbde0c1dc1ff5ebc27bb18777ac6d73012b6
naimkim <naimk5451@gmail.com>
2025-03-07 22:03:58 +0900

    STYLE 탈퇴 페이지 디자인 완

--- root_app/lib/screens/my_page/delete_page.dart
+++ root_app/lib/screens/my_page/delete_page.dart
@@ -8,20 +8,16 @@ import 'package:root_app/utils/icon_paths.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class DeletePage extends StatelessWidget {
-  // Get the userId from the navigation arguments
   final String userId = Get.arguments['userId'];
 
   DeletePage({Key? key}) : super(key: key);
 
-  // Function to handle account deletion
   Future<void> _confirmDeletion(BuildContext context) async {
     bool success = await ApiService.deleteUser(userId);
     if (success) {
-      Get.offAllNamed('/signin'); // Navigate to sign-in if successful
+      Get.offAllNamed('/signin');
     } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('계정 삭제에 실패했습니다. 다시 시도해 주세요.')),
-      );
+      Get.offAllNamed('/signin');
     }
   }
 
@@ -52,105 +48,109 @@ class DeletePage extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.all(20.w),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // First Text Part
-                Text.rich(
-                  TextSpan(
-                    text: '탈퇴 하시면, 지금까지 저장한\n모든 콘텐츠가',
-                    style: TextStyle(
-                      fontSize: 22.sp,
-                      fontFamily: 'Five',
-                      height: 1.5,
-                    ),
-                    children: [
-                      TextSpan(
-                        text: ' 영영 사라져요 :(',
-                        style: TextStyle(
-                          color: Colors.red,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  textAlign: TextAlign.left,
-                ),
-                SizedBox(height: 5.h),
-
-// Second Text Part
-                Text(
-                  '탈퇴 시 계정 및 이용 기록은 모두 삭제되며,\n삭제된 데이터는 복구가 불가능해요.',
-                  style: TextStyle(
-                    fontSize: 15.sp,
-                    fontFamily: 'Three',
-                    color: Colors.grey,
-                    height: 1.5,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-            SizedBox(height: 40.h),
-            InkWell(
-              onTap: () {
-                _launchURL('https://tally.so/r/mBjO91');
-              },
-              child: Container(
-                height: 87,
-                width: 350.w,
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Color(0xFFF5F6FA),
-                  borderRadius: BorderRadius.circular(12.r),
-                ),
-                child: Row(
-                  children: [
-                    Text(
-                      '전달하고 싶은 피드백이 있나요?\n피드백 창구를 활용해보세요!',
+            // Top Content
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // 큰
+                  Text.rich(
+                    TextSpan(
+                      text: '탈퇴 하시면, 지금까지 저장한\n모든 콘텐츠가',
                       style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 14,
-                        fontFamily: 'Six',
+                        fontSize: 24,
+                        fontFamily: 'Seven',
+                        height: 1.25,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: ' 영영 사라져요 :(',
+                          style: TextStyle(
+                            color: Color(0xFFCD423D),
+                          ),
+                        ),
+                      ],
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                  SizedBox(height: 16),
+
+                  // 회색 글
+                  Text(
+                    '탈퇴 시 계정 및 이용 기록은 모두 삭제되며,\n삭제된 데이터는 복구가 불가능해요.',
+                    style: TextStyle(
+                      fontSize: 13,
+                      fontFamily: 'Five',
+                      color: Colors.grey,
+                      height: 1.25,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                  SizedBox(height: 40.h),
+
+                  // 피드백 컨테이너띠띠
+                  InkWell(
+                    onTap: () {
+                      _launchURL('https://tally.so/r/mBjO91');
+                    },
+                    child: Container(
+                      height: 87,
+                      width: 350.w,
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF5F6FA),
+                        borderRadius: BorderRadius.circular(12.r),
+                      ),
+                      child: Row(
+                        children: [
+                          SvgPicture.asset(IconPaths.getIcon('my_logo')),
+                          SizedBox(width: 13.w),
+                          Text.rich(
+                            TextSpan(
+                              text: '루트를 탈퇴하시는 이유를 알려주세요.',
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontFamily: 'Six',
+                              ),
+                              children: [
+                                TextSpan(
+                                  text: '\n더 개선된 루트로 돌아올게요!',
+                                  style: TextStyle(
+                                    fontSize: 13,
+                                    fontFamily: 'Four',
+                                  ),
+                                ),
+                              ],
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                          Spacer(),
+                          SvgPicture.asset(
+                              IconPaths.getIcon('double_arrow_dark'),
+                              fit: BoxFit.contain),
+                        ],
                       ),
                     ),
-                    Spacer(),
-                    Image.asset('assets/icons/message.png'),
-                    SizedBox(width: 16.w),
-                    SvgPicture.asset(IconPaths.getIcon('double_arrow'),
-                        fit: BoxFit.contain),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(height: 41.h),
-            Text(
-              '이용 안내',
-              style: TextStyle(
-                fontSize: 15,
-                fontFamily: 'Five',
-                color: Colors.grey,
-              ),
-            ),
-            SizedBox(height: 40.h),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () => _confirmDeletion(context),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 36, 33, 219),
-                    padding:
-                        EdgeInsets.symmetric(vertical: 12.h, horizontal: 40.w),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.r),
-                    ),
                   ),
-                  child: Text('탈퇴하기', style: TextStyle(color: Colors.white)),
+                ],
+              ),
+            ),
+
+            // 맨 및 탈퇴하기 button
+            Padding(
+              padding: EdgeInsets.only(bottom: 19.h),
+              child: ElevatedButton(
+                onPressed: () => _confirmDeletion(context),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF2960C6),
+                  fixedSize: Size(284, 60),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(100.r),
+                  ),
                 ),
-              ],
+                child: Text('탈퇴하기', style: TextStyle(color: Colors.white)),
+              ),
             ),
           ],
         ),
