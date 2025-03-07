02afaa17abe7b9b96ce0fc1881c90d8c09786c81
naimkim <naimk5451@gmail.com>
2025-03-07 23:15:52 +0900

    STYLE add tutorial svg

--- root_app/lib/utils/icon_paths.dart
+++ root_app/lib/utils/icon_paths.dart
@@ -28,6 +28,7 @@ class IconPaths {
   static const String pencil = '${basePath}pencil.svg';
   static const String check = '${basePath}check.svg';
   static const String root = '${basePath}ROOT.svg';
+  static const String tutorial = '${basePath}tutorial.svg';
 
   static String getIcon(String iconName) {
     return '$basePath$iconName.svg';
